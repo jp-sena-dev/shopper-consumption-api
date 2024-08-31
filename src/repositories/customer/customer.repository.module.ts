659a29ddbc3eb423694128ba8102a2d452062164
jp-sena-dev <joaopedrosena1997@gmail.com>
2024-08-30 21:24:55 -0300

    :bug: fix: import in customer repository

--- src/repositories/customer/customer.repository.module.ts
+++ src/repositories/customer/customer.repository.module.ts
@@ -1,7 +1,7 @@
-import { CustomerRepository } from '@interfaces/measurement/customer.repository';
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@repositories/prisma.service';
 import { PrismaCustomerRepository } from './prisma-customer.repository';
+import { CustomerRepository } from '@interfaces/customer/customer-repository.repository';
 
 @Module({
   providers: [
